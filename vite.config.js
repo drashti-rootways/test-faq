53ef7451a6ba2a5465d26aaa9b75233eb5608022
Drashti Patel <drashti@rootways.com>
2025-07-14 15:07:30 +0530

    modified vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
+import { vitePlugin as remix } from "@remix-run/dev";
 import { vercelPreset } from '@vercel/remix/vite';
+
 import tsconfigPaths from "vite-tsconfig-paths";
 import { installGlobals } from "@remix-run/node";
 
